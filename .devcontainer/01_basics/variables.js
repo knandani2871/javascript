40a65360c346335c799c3d717cd085dca07d157b
Nandani Kumari <knandani2871@gmail.com>
2025-06-24 15:43:57 +0000

    new config

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 2486
+let accountEmail = "nk2871@gmail.com"
+var accountPassword = "12345"
+accountCity = "patna"
+let accountState;
+//accountId = 2 // not allowed
+accountEmail = "kn2871@gmail.com"
+accountPassword = "9876"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.table([accountId,accountState, accountEmail,accountPassword, accountCity])
